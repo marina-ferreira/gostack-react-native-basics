086590411f0d74bbe9a8faf6d9d55a7830586824
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-21 09:04:09 -0300

    List projects

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native'
+
+import api from './services/api'
 
 const App = () => {
-  return <View />
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    api.get('/projects').then(response => {
+      setProjects(response.data)
+    })
+  }, [])
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.title}>{project.title}</Text>
+          )}
+        />
+      </SafeAreaView>
+    </>
+  )
 }
 
 export default App
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+  },
+  title: {
+    color: '#f5f5f5',
+    fontSize: 32,
+    fontWeight: 'bold'
+  }
+})
